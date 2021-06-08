a8440ebc1f054c444792d2b3acac91909106c3f1
bklring <1115913658@qq.com>
2021-06-08 17:34:56 +0800

    配置eureka的server端与client端

--- cloud-provider-payment8001/src/main/java/com/kevin/springcloud/PaymentMain8001.java
+++ cloud-provider-payment8001/src/main/java/com/kevin/springcloud/PaymentMain8001.java
@@ -2,10 +2,13 @@ package com.kevin.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class PaymentMain8001 {
     public static void main(String[] args) {
+
         SpringApplication.run(PaymentMain8001.class,args);
     }
 }
